e0753fc2bc8487ad6413de00097d5094866b2c61
Duygu GÜNGÖR <45100700+duygugungor@users.noreply.github.com>
2020-09-22 14:51:26 +0300

    Add project files.

---
+++ BlazorApp1/Client/Data/BoldReportOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Client.Data
+{
+    public class BoldReportOptions
+    {
+    }
+    public class BoldReportViewerOptions
+    {
+        public string ReportName { get; set; }
+        public string ServiceURL { get; set; }
+    }
+}
